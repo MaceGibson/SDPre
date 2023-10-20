1b0e25a015e2c21b5439a9592e6436a3ac85aa06
MaceGibson <mace_gibson@yahoo.com>
2023-10-20 15:24:58 -0400

    objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,15 @@
+public class Objective8Lab1 {
+	public static void main(String[] args) {
+
+	for (int i = 1; i <= 10; i++) {
+		int num1 = i;
+		System.out.println(num1);
+	}
+
+	for (int i = 10; i >= 1; i--) {
+		int num2 = i;
+		System.out.println(num2);
+	}
+
+	}
+}
