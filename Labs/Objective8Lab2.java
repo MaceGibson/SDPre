f426b2857ee740bef4cd25166e43ec49c5217f55
MaceGibson <mace_gibson@yahoo.com>
2023-10-20 15:50:21 -0400

    objective8lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,11 @@
+public class Objective8Lab2 {
+	public static void main(String[] args) {
+		int total = 0;
+
+		for (int i = 0; i <= 20; i++) {
+			total+= i;
+		}
+
+		System.out.println(total);
+	}
+}
